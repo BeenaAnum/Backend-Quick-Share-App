8ca7b27c8f3c9f5836257e92add76d08985c0b6f
AI Anam <160389592+BeenaAnum@users.noreply.github.com>
2026-05-08 16:48:59 +0500

    Export app for Vercel deployment
    Added module.exports for Vercel deployment.

--- index.js
+++ index.js
@@ -46,3 +46,4 @@ res.status(500).json({ error: "Save Error" });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app; //important for vercel deployment 
